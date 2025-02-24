14597625fd48b523f52998aa2ad651551b457cc1
Jean Junior <jclgjunior@gmail.com>
2025-02-24 17:44:31 -0300

    feat: generate JWT

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,20 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async signIn(username: string, password: string) {
     const user = await this.userService.findByUsername(username);
     // TODO: Create a hash of the password and compare it with the hash stored in the database
-    if (user.password === password) {
-      return user;
+    if (user.password !== password) {
+      throw new UnauthorizedException();
     }
 
-    // TODO: Generate a JWT token and return it
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { password: _, ...result } = user;
-    return result;
+    const jwtPayload = { username: user.username, sub: user.id };
+
+    return {
+      access_token: await this.jwtService.signAsync(jwtPayload),
+    };
   }
 }
